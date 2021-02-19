a205916d12b490e5e69e17dcd57d9dba56fa077f
Julia Stelman <stelman@ucdavis.edu>
2021-02-19 14:35:44 -0800

    debug mode added to shorten test runs. First successful pyabc_construct test run w/ no error!

--- src/simulation_procedure.py
+++ src/simulation_procedure.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 import os, pandas as pd, numpy as np, pickle
 import pytest_shutil, shutil, regex as re, dask, uuid
 from pyswmm import Simulation
-from pyswmm.swmm5 import SWMMException
 import swmmtoolbox.swmmtoolbox as swmmtoolbox
 import time
 from pyswmm.lib import DLL_SELECTION
@@ -19,19 +18,19 @@ import subprocess
 # #### Set mode: 'test' or 'run'
 # Test mode is designed to test code quickly by using a small sample of the data instead of the whole thing
 
-# In[4]:
+# In[2]:
 
 
 # activate 
-mode = 'test'
+mode = 'debug'
+# mode = 'test'
 # mode = 'run'
 
 
-# In[5]:
+# In[3]:
 
 
 from tools.paths import *
-
 from tools.functions import *
 
 # this is the path to the default dynamic link library used for running SWMM
@@ -40,13 +39,16 @@ dll_path = DLL_SELECTION()
 dll_bn = os.path.basename(dll_path)
 
 
-# In[6]:
+# In[4]:
 
 
 # the 7 outfall names
 # used by 05+ 
-outfalls = ['outfall_31_26', 'outfall_31_28', 'outfall_31_29', 'outfall_31_35',
-            'outfall_31_36', 'outfall_31_38', 'outfall_31_42']
+if mode == "debug":
+    outfalls = ['outfall_31_28']
+else:
+    outfalls = ['outfall_31_26', 'outfall_31_28', 'outfall_31_29', 'outfall_31_35',
+                'outfall_31_36', 'outfall_31_38', 'outfall_31_42']
 
 # sub_list_area has the areas of each subcatchment in order
 # used by 05
@@ -59,7 +61,7 @@ with open(os.path.join(master_path,'outfall_partition.txt'),'r') as read_file:
     sub_ids = eval(read_file.read())
 
 
-# In[7]:
+# In[5]:
 
 
 # activate test mode
@@ -69,7 +71,7 @@ print(inp_path)
 
 # ## Set up input variables and run id
 
-# In[8]:
+# In[6]:
 
 
 '''
@@ -77,37 +79,48 @@ print(inp_path)
 MAKE SURE TO SWITCH WP AND FC IN THE FILE! THEY ARE BACKWARDS AND IT'S ANNOYING
 '''
 
-# In[25]:
+
+# In[7]:
 
 
 # Import Observed Data
 obs_data = pd.read_csv(obs_path, usecols=["Sample_date", "Site_code"],
                        parse_dates=["Sample_date"])
 
-
 def model(params1):
-    
-    # error prevention
-    if params1['MaxRate'] < params1['MinRate']:
-        params1['MaxRate'], params1['MinRate'] = params1['MinRate'], params1['MaxRate']
-    if params1['FC'] < params1['WP']:
-        params1['FC'], params1['WP'] = params1['WP'], params1['FC']
-    
-    
+
+    # import test parameter input
+    if mode == "debug":
+        with open(os.path.join(main_path,'master_debug','debug_params.txt'),'r') as read_file:
+            params1 = eval(read_file.read())
+        # get them into the objects we need
+        swmm_keys = list(params1.keys())[:1]
+        vvwm_keys = list(params1.keys())[1:]
+        swmm_params = {key: params1[key] for key in swmm_keys}
+        vvwm_params = {key: params1[key] for key in vvwm_keys}
+        
+    elif mode == "test":
+        with open(os.path.join(main_path,'master_test','test_params.txt'),'r') as read_file:
+            params1 = eval(read_file.read())
+        
+        # error prevention
+        if params1['MaxRate'] < params1['MinRate']:
+            params1['MaxRate'], params1['MinRate'] = params1['MinRate'], params1['MaxRate']
+        if params1['FC'] < params1['WP']:
+            params1['FC'], params1['WP'] = params1['WP'], params1['FC']
+            
+        # get them into the objects we need
+        swmm_keys = list(params1.keys())[:17]
+
+        # wp and fc in this object should be switched. FOR NOW:
+        swmm_keys = swmm_keys[:10] + ['WP', 'FC'] + swmm_keys[12:]
+
+        vvwm_keys = list(params1.keys())[17:]
+        swmm_params = {key: params1[key] for key in swmm_keys}
+        vvwm_params = {key: params1[key] for key in vvwm_keys}
+
+
     # In[9]:
-    
-    # get them into the objects we need
-    swmm_keys = list(params1.keys())[:17]
-
-    # wp and fc in this object should be switched. FOR NOW:
-    swmm_keys = swmm_keys[:10] + ['WP', 'FC'] + swmm_keys[12:]
-
-    vvwm_keys = list(params1.keys())[17:]
-    swmm_params = {key: params1[key] for key in swmm_keys}
-    vvwm_params = {key: params1[key] for key in vvwm_keys}
-
-
-    # In[10]:
 
 
     # spin up simulation id with this cool too for generating random ids
@@ -117,7 +130,7 @@ def model(params1):
 
     # ## Step 1: make simulation-specific SWMM items
 
-    # In[11]:
+    # In[10]:
 
 
     '''
@@ -125,7 +138,7 @@ def model(params1):
     '''
 
 
-    # In[12]:
+    # In[11]:
 
 
     # make paths to (soon-to-be) directory & files for this simulation using sid
@@ -145,7 +158,7 @@ def model(params1):
     sdll_path = os.path.join(sdir_path, sdll_bn) 
 
 
-    # In[13]:
+    # In[12]:
 
 
     # make the directory
@@ -161,27 +174,31 @@ def model(params1):
 
         # first we need to correct some absolute paths, because they are currently only set to work on the author's computer
         filelines = replace_infile_abspaths(filelines = filelines)
+    
+        if mode == "debug":
+            filelines[172:(172 + 113)] = editted_lines(swmm_dict = swmm_params, Num = 113, row_0 = 172, parameter = "NImperv", Col = 1, flines = filelines)
         
-        # 113 = number of subcatchments
-        #for c, par in enumerate(["NImperv", "NPerv", "SImperv", "SPerv", "PctZero"]):
-        for c, par in enumerate(swmm_keys[0:5]):
-            filelines[172:(172 + 113)] = editted_lines(swmm_dict = swmm_params, Num = 113, row_0 = 172, parameter = par, Col = c+1, flines = filelines)
-        #for c, par in enumerate(["MaxRate", "MinRate", "Decay", "DryTime"]):
-        for c, par in enumerate(swmm_keys[5:9]):
-            filelines[289:(289 + 113)] = editted_lines(swmm_dict = swmm_params, Num = 113, row_0 = 289, parameter = par, Col = c+1, flines = filelines)
+        elif mode == "test":
+            # 113 = number of subcatchments
+            #for c, par in enumerate(["NImperv", "NPerv", "SImperv", "SPerv", "PctZero"]):
+            for c, par in enumerate(swmm_keys[0:5]):
+                filelines[172:(172 + 113)] = editted_lines(swmm_dict = swmm_params, Num = 113, row_0 = 172, parameter = par, Col = c+1, flines = filelines)
+            #for c, par in enumerate(["MaxRate", "MinRate", "Decay", "DryTime"]):
+            for c, par in enumerate(swmm_keys[5:9]):
+                filelines[289:(289 + 113)] = editted_lines(swmm_dict = swmm_params, Num = 113, row_0 = 289, parameter = par, Col = c+1, flines = filelines)
 
-        # 1 = number of aquifers
-        #for c, par in enumerate(["Por", "WP", "FC", "Ksat"]):
-        for c, par in enumerate(swmm_keys[9:13]):
-            filelines[406:(406 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 406, parameter = par, Col = c+1, flines = filelines)
+            # 1 = number of aquifers
+            #for c, par in enumerate(["Por", "WP", "FC", "Ksat"]):
+            for c, par in enumerate(swmm_keys[9:13]):
+                filelines[406:(406 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 406, parameter = par, Col = c+1, flines = filelines)
 
-        # 195 = number of conduits
-        # filelines[734:(734 + 195)] = editted_lines(swmm_dict = swmm_params, Num = 195, row_0 = 734, parameter = "Rough", Col = 4, flines = filelines)
+            # 195 = number of conduits
+            # filelines[734:(734 + 195)] = editted_lines(swmm_dict = swmm_params, Num = 195, row_0 = 734, parameter = "Rough", Col = 4, flines = filelines)
 
-        # 1 = number of pollutants
-        filelines[1125:(1125 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 1125, parameter = "Kdecay", Col = 5, flines = filelines)
-        filelines[1371:(1371 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 1371, parameter = "BCoeff2", Col = 4, flines = filelines)
-        filelines[1377:(1377 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 1377, parameter = "WCoeff2", Col = 4, flines = filelines)
+            # 1 = number of pollutants
+            filelines[1125:(1125 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 1125, parameter = "Kdecay", Col = 5, flines = filelines)
+            filelines[1371:(1371 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 1371, parameter = "BCoeff2", Col = 4, flines = filelines)
+            filelines[1377:(1377 + 1)] = editted_lines(swmm_dict = swmm_params, Num = 1, row_0 = 1377, parameter = "WCoeff2", Col = 4, flines = filelines)
         
         write_file.writelines(filelines)
 
@@ -193,7 +210,7 @@ def model(params1):
     # In test mode, should take about 2-3 minutes. 
     # In run mode, should take a while.
 
-    # In[14]:
+    # In[13]:
 
 
     # set up logger
@@ -209,51 +226,15 @@ def model(params1):
 
     sim = Simulation(inputfile=sinp_path, reportfile=srpt_path, outputfile=sout_path, swmm_lib_path=sdll_path)
     # if no errors were thrown, we procede with the simulation
-
     # simulate the loaded model
     loginfo("Executing SWMM simmulation with no interaction. Input from <" + sinp_path + ">. Will store output in <" + sout_path + ">.")
     with sim as s:
         for step in s:
             pass
-    # try:
-    #     # the error should happen at these two lines if ever
-    #     if params1['MaxRate'] < params1['MinRate']:
-    #         raise SWMMException(error_code = 200, error_message = "MaxRate < MinRate")
-    #     if params1['FC'] < params1['WP']:
-    #         raise SWMMException(error_code = 200, error_message = "FC < WP")
-    #     # if the problem is somewhere else, this will trigger it
-    #     sim = Simulation(inputfile=sinp_path, reportfile=srpt_path, outputfile=sout_path, swmm_lib_path=sdll_path)
-    #     # if no errors were thrown, we procede with the simulation
-
-    #     # simulate the loaded model
-    #     loginfo("Executing SWMM simmulation with no interaction. Input from <" + sinp_path + ">. Will store output in <" + sout_path + ">.")
-    #     with sim as s:
-    #         for step in s:
-    #             pass
-
-    # except SWMMException as err:
-    #     print(err)
-    #     logerror("Error in  input from <" + sinp_path + ">. " + str(err))
-    #     print(swmm_params)
-    #     if mode == 'test':
-    #         with open(os.path.join(main_path,'master_test','test_err_handling.pkl'),'rb') as read_pkl:
-    #             output_dict = pickle.load(read_pkl)
-            
-    #     elif mode == 'run':
-    #         with open(os.path.join(master_path,'err_handling.pkl'),'rb') as read_pkl:
-    #             output_dict = pickle.load(read_pkl)
-
-    #     # cleanup
-    #     # print(os.remove(sinp_path))
-    #     # print(os.system("rm -f " + sinp_path))
-    #     # print(os.system("rm -f " + sdll_path))
-    #     print(os.system("rm -r " + sdir_path))
-    #     # return dictionary of nans
-    #     return(output_dict)
-
+        
     # #### Get the info to a safe place and then delete the whole temp folder 
 
-    # In[15]:
+    # In[14]:
 
 
     # extract swmm outputs with swmmtoolbox and delete expensive binary files
@@ -265,7 +246,7 @@ def model(params1):
     os.system("rm " + sdir_path + "/*")
 
 
-    # In[16]:
+    # In[15]:
 
 
     # compute daily averages
@@ -273,7 +254,7 @@ def model(params1):
     bif = bif.resample('D').mean()
 
 
-    # In[17]:
+    # In[16]:
 
 
     # conversion for vvwm for runoff and bifenthrin
@@ -281,7 +262,7 @@ def model(params1):
     bif = bif.mul(runf.values)
 
 
-    # In[18]:
+    # In[17]:
 
 
     for o in outfalls:
@@ -329,7 +310,7 @@ def model(params1):
             write_file.writelines(filelines) #JMS 10-21-20
 
 
-    # In[88]:
+    # In[18]:
 
 
     # create a blank df to populate
@@ -343,16 +324,22 @@ def model(params1):
         with open(vvwmTransfer_path,"r") as read_file:
             filelines = read_file.readlines()
         
-        for c, param in enumerate(list(vvwm_keys)[1:8]):
-            filelines[c+4] = str(vvwm_params[param]) + "\n"
-        
-        filelines[17] = str(vvwm_params[vvwm_keys[8]]) + "\n"
+        if mode == "debug":
+            filelines[4] = str(vvwm_params[vvwm_keys[0]]) + "\n"
+            
+        elif mode == "test":
 
-        for c, param in enumerate(list(vvwm_keys)[9:15]):
-            filelines[c+40] = str(vvwm_params[param]) + "\n"
+            # why is it 1:8 and not 0:8? Is it a typo?
+            for c, param in enumerate(list(vvwm_keys)[0:8]): # changed from 1:8 2/19/21
+                filelines[c+4] = str(vvwm_params[param]) + "\n"
 
-        for c, param in enumerate(list(vvwm_keys)[15:19]):
-            filelines[c+47] = str(vvwm_params[param]) + "\n"
+            filelines[17] = str(vvwm_params[vvwm_keys[8]]) + "\n"
+
+            for c, param in enumerate(list(vvwm_keys)[9:15]):
+                filelines[c+40] = str(vvwm_params[param]) + "\n"
+
+            for c, param in enumerate(list(vvwm_keys)[15:19]):
+                filelines[c+47] = str(vvwm_params[param]) + "\n"
 
         # enter script 9
         '''
@@ -386,7 +373,9 @@ def model(params1):
             write_file.writelines(filelines)
         
         # copy weather file into new file location
-        if mode == 'test':
+        if mode == 'debug':
+            old_wet_path = os.path.join(main_path, "master_debug", "vvwm_wet.dvf")
+        elif mode == 'test':
             old_wet_path = os.path.join(main_path, "master_test", "vvwm_wet.dvf")
         elif mode =='run':
             old_wet_path = os.path.join(weather_path, "vvwm_wet.dvf")
@@ -396,21 +385,23 @@ def model(params1):
         # copy exe into new file location
         if sys.platform == "linux" or sys.platform == "linux2":
             exe_bn = "vvwm"
-        if sys.platform == "win32":
+        elif sys.platform == "win32":
             exe_bn = "VVWM.exe"
         old_exe_path = os.path.join(exe_path, exe_bn)
-        new_exe_path = os.path.join(outfall_dir, exe_bn)
+        new_exe_path = os.path.join(outfall_dir, "VVWM.exe")
         shutil.copyfile(old_exe_path, new_exe_path)
         
         # run vvwm.exe (vvwm.exe [...]/outfall_31_xx/vvwmTransfer.txt)
         command = new_exe_path + ' ' + outfall_file
         subprocess.call(command)
-        
+    
         # read in produced data from the output of the vvwm run we just completed
         output = pd.read_csv(filelines[68][:-1], 
                             usecols = [1], skiprows=5, names = ["davg_bif_conc"])*1000000
-        if mode == 'test':
-            output['Sample_date'] = pd.date_range(start='2/12/2009', periods=736, freq='D')
+        if mode == 'debug':
+            output['Sample_date'] = pd.date_range(start='1/1/2009', periods=103, freq='D')
+        elif mode == 'test':
+            output['Sample_date'] = pd.date_range(start='1/1/2009', periods=778, freq='D')
         elif mode == 'run':
             output['Sample_date'] = pd.date_range(start='1/1/2009', periods=3287, freq='D')
         output['Site_code'] = o[-5:]
@@ -422,16 +413,23 @@ def model(params1):
     output_df = output_df.set_index([["_".join([a,b[3:]]) for a,b in output_df.index]]).sort_index()
 
 
-    # In[92]:
+    # In[19]:
 
 
     output_dict = output_df.to_dict()['davg_bif_conc']
 
 
-    # In[ ]:
+    # In[20]:
 
-    # os.system("rm -r " + sdir_path + "/")
+
+    os.system("rm -r " + sdir_path + "/")
+
+
+    # In[22]:
+
+
     return(output_dict)
+    # output_dict
 
 
 # ### We made it to the post-processing stage!
